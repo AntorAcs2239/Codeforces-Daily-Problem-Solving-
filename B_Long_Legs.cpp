74f330e6afef086cac31ca9abb5461badde87b1b
Antor Acs <antorsarkeracs@gmail.com>
2023-04-08 20:57:02 +0600

    Date 8/4/23

---
+++ B_Long_Legs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    ll ans=LLONG_MAX;
+    for(int i=1;i<=100005;i++)
+    {
+        ll x=(a+i-1)/i;
+        ll y=(b+i-1)/i;
+        ll z=i-1+x+y;
+        ans=min(ans,z);
+    }
+    cout<<ans<<endl;
+
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
