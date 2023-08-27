9602cd1c924ee5e70c1d1ff04afe02587543860d
Antor Acs <antorsarkeracs@gmail.com>
2023-08-27 08:03:54 +0600

    Date 27/8/23

--- D_Ice_Cream_Balls.cpp
+++ D_Ice_Cream_Balls.cpp
@@ -3,43 +3,44 @@ using namespace std;
 #define ll long long int
 ll s(ll n)
 {
-    ll ans=1;
-    if(n%2==0)
+    ll ans = 1;
+    if (n % 2 == 0)
     {
-        ans=(n/2)*(n-1);
+        ans = (n / 2) * (n - 1);
     }
-    else 
+    else
     {
-        ans=((n-1)/2)*n;
+        ans = ((n - 1) / 2) * n;
     }
     return ans;
 }
 void solve()
 {
     ll n;
-    cin>>n;
-    ll left=1,right=(2e9);
+    cin >> n;
+    ll left = 1, right = (2e9);
     ll mid;
     ll p;
-    while(left<=right)
+    while (left <= right)
     {
-        mid=(left+right)/2;
-        ll x=s(mid);
-        if(x==n)
+        mid = (left + right) / 2;
+        ll x = s(mid);
+        if (x == n)
         {
-            p=mid;
+            p = mid;
             break;
         }
-        else if(x<n)
+        else if (x < n)
         {
-            p=mid;
-            left=mid+1;
+            p = mid;
+            left = mid + 1;
         }
-        else right=mid-1;
+        else
+            right = mid - 1;
     }
-    ll x=s(p);
-    x=n-x;
-    cout<<p+x<<endl;
+    ll x = s(p);
+    x = n - x;
+    cout << p + x << endl;
 }
 int main()
 {
